ab1467ee14ddd21078b280622647d2633b5ceddf
josejavierpaez <josejavierpaez@hotmail.com>
2021-10-19 18:55:44 -0400

    ✨ feature(component): create card component and add it to hotels pages

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Hotels} from './pages/Hotels';
-import {NavBar} from './components/navbar/NavBar';
+
 export const AppRouter = () => {
   return (
     <div>
       <Router>
-        <NavBar />
         <Switch>
           <Route path='/' component={Hotels} />
         </Switch>
